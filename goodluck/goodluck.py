457da0c51459a5c7bf77b87317e0d7735c6cfe3a
KaneSuperMan <kane.superman@outlook.com>
2023-04-15 10:50:56 +0800

    添加goodluck

---
+++ goodluck/goodluck.py
@@ -0,0 +1,21 @@
+import random
+
+My_Ball = []
+while True:
+    # 生成一位随机数
+    Data = random.randint(1, 34)
+
+    # 避免重复
+    if Data not in My_Ball:
+        # 把不重复的数字，添加到列表
+        My_Ball.append(Data)
+
+        # 返回6个不重复的红球
+        if len(My_Ball) == 6:
+            break
+
+# 生成蓝球
+My_Ball.sort()
+Blue_Ball = random.randint(1, 17)
+My_Ball.append(Blue_Ball)
+print("本次号码:", My_Ball)
