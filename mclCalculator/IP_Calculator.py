08479304bcd70e1b2889ca49de008ab1efc605a3
KaneSuperMan <kane.superman@outlook.com>
2023-04-26 23:55:21 +0800

    更改为循环输入

--- mclCalculator/IP_Calculator.py
+++ mclCalculator/IP_Calculator.py
@@ -2,6 +2,7 @@ import sys
 import signal
 import ipaddress
 
+
 def IP_Calculator(ip_net):
     try:
         net = ipaddress.ip_network(ip_net, strict=False)
@@ -15,16 +16,23 @@ def IP_Calculator(ip_net):
         print('可用IP地址总数： ' + str(len([x for x in net.hosts()])))
         print('起始可用IP地址： ' + str([x for x in net.hosts()][0]))
         print('最后可用IP地址： ' + str([x for x in net.hosts()][-1]))
-        print('可用IP地址范围： ' + str([x for x in net.hosts()][0]) + ' ~ ' + str([x for x in net.hosts()][-1]))
+        print('可用IP地址范围： ' + str([x for x in net.hosts()]
+              [0]) + ' ~ ' + str([x for x in net.hosts()][-1]))
         print('广播地址： ' + str(net.broadcast_address))
     except ValueError:
-        print('您输入格式有误，请检查！')
+        print('您输入格式有误，请检查！\n')
 
-def quit(signum,frame):
+
+def quit(signum, frame):
     print("\nBye!")
     sys.exit(0)
 
+
 if __name__ == '__main__':
-    signal.signal(signal.SIGINT,quit)
-    ip_net = input("请输入IP地址/掩码：").replace(" ","")
-    IP_Calculator(ip_net)
+    signal.signal(signal.SIGINT, quit)
+    print("""Welcome!\nCTRL+C退出程序\n输入地址格式: IP地址/掩码; 输入样例如下: \n192.168.1.0/24或192.168.1.0/255.255.255.0\n""")
+    while True:
+        ip_net = input("请输入: ").replace(" ", "")
+        print("****************************************************")
+        IP_Calculator(ip_net)
+        print("****************************************************\n")
